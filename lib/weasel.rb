a82bbf83c285de0775cbe0a3a137148e1106d047
Horacio Bertorello <syrii@msn.com>
2016-03-04 16:38:05 -0300

    Add a simple Configuration class.

--- lib/weasel.rb
+++ lib/weasel.rb
@@ -7,4 +7,18 @@ module Weasel
   def self.root
     File.expand_path('../..', __FILE__)
   end
+
+  class Configuration
+    attr_accessor :db_configuration
+  end
+
+  def self.config
+    @@config ||= Configuration.new
+  end
+
+  def self.configure
+    yield config
+
+    ActiveRecord::Base.establish_connection(config.db_configuration)
+  end
 end
