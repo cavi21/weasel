d64d3a6b86f099b3058ec0ea53cd795b05dde3cc
Horacio Bertorello <syrii@msn.com>
2016-04-20 13:33:36 -0300

    Fix GeoIP calls.

--- lib/weasel/concerns/auditable.rb
+++ lib/weasel/concerns/auditable.rb
@@ -1,10 +1,10 @@
 module Weasel
   def audit_with_weasel(*args)
-    around_filter :thing, only: args
+    around_filter :audit, only: args
   end
 
   module Auditable
-    def thing
+    def audit
       yield
 
       Weasel::Event.create! do |object|
@@ -16,21 +16,22 @@ module Weasel
         object.action_data = {
           action: rails_action,
           params: rails_parameters,
-          flash: request.flash,
           remote_ip: request.remote_ip,
-          geo_ip: geoip.city(request.remote_ip).to_h,
+          country: geo_service.country(request.remote_ip).to_h,
           user_agent: request.user_agent,
           response: {
             status: response.status
           }.tap do |hash|
             hash[:body] = response.body if response.body
           end
-        }
+        }.tap do |hash|
+          hash[:flash] = request.flash if request.flash
+        end
       end
+    end
 
-      def geoip
-        @@geoip ||= GeoIP.new(File.join(Rails.root, 'tmp/GeoIP.dat'))
-      end
+    def geo_service
+      @@geoip ||= GeoIP.new(File.join(Rails.root, 'tmp/GeoIP.dat'))
     end
   end
 end
