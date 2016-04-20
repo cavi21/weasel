6d696c458bc275694a87a203b6ac59e092bf676e
Horacio Bertorello <syrii@msn.com>
2016-04-20 12:52:32 -0300

    Adds user agent and GeoIP information. Closes #1.

--- lib/weasel/concerns/auditable.rb
+++ lib/weasel/concerns/auditable.rb
@@ -9,10 +9,17 @@ module Weasel
 
       Weasel::Event.create! do |object|
         object.actor = current_user
+
+        rails_action = "#{params[:controller]}:#{params[:action]}"
+        rails_parameters = params.except(:controller, :action)
+
         object.action_data = {
+          action: rails_action,
+          params: rails_parameters,
           flash: request.flash,
-          params: request.params,
           remote_ip: request.remote_ip,
+          geo_ip: geoip.city(request.remote_ip).to_h,
+          user_agent: request.user_agent,
           response: {
             status: response.status
           }.tap do |hash|
@@ -20,7 +27,10 @@ module Weasel
           end
         }
       end
-    end
 
+      def geoip
+        @@geoip ||= GeoIP.new(File.join(Rails.root, 'tmp/GeoIP.dat'))
+      end
+    end
   end
 end
